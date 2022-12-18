1c8714107598af7f3bb5a22a7f9b7f39de88144a
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-18 11:18:35 +0100

    chore: add babel plugin for resolving relative addresses, animation, replacing console logs, optional requiring packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
 };
+
+module.exports = api => {
+  const babelEnv = api.env();
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['./src'],
+      },
+    ],
+    // ['module:react-native-dotenv'],
+    ['react-native-paper/babel'],
+  ];
+  //change to 'production' to check if this is working in 'development' mode
+  if (babelEnv !== 'development') {
+    plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [...plugins, 'optional-require', 'react-native-reanimated/plugin'],
+  };
+};
