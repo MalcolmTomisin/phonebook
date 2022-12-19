7c715babc54e6886a133a09effa9decd415d24e4
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-19 22:05:56 +0100

    feat: list of contacts - add theme for dark and light contexts

---
+++ src/config/theme.ts
@@ -0,0 +1,72 @@
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
+import {globalDimens} from 'globalstyles';
+
+const theme = {
+  ...PaperDefaultTheme,
+  roundness: (8 / 375) * globalDimens.DEVICE_WIDTH,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    primary: '#DE774C',
+    accent: '#A08673',
+    primaryInactive: '#E88C51',
+    background: '#FFFFFF',
+  },
+  fonts: {
+    regular: {
+      fontFamily: 'Montserrat-Regular',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'Montserrat-Medium',
+      fontWeight: 'normal',
+    },
+    bold: {
+      fontFamily: 'Montserrat-Bold',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'Montserrat-Light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'Montserrat-Thin',
+      fontWeight: 'normal',
+    },
+  },
+};
+
+const darkTheme = {
+  ...PaperDarkTheme,
+  roundness: (8 / 375) * globalDimens.DEVICE_WIDTH,
+  colors: {
+    ...PaperDarkTheme.colors,
+    primary: '#DE774C',
+    accent: '#A08673',
+    primaryInactive: '#E88C51',
+  },
+};
+
+export const CombinedDefaultTheme = {
+  ...NavigationDefaultTheme,
+  ...theme,
+  colors: {
+    ...NavigationDefaultTheme.colors,
+    ...theme.colors,
+  },
+};
+export const CombinedDarkTheme = {
+  ...NavigationDarkTheme,
+  ...darkTheme,
+  colors: {
+    ...NavigationDarkTheme.colors,
+    ...darkTheme.colors,
+    background: '#000000',
+  },
+};
